af3a2631ae873cdefb3ef85619f21075e19de083
Darryl Kilzer <darrylkilzer@gmail.com>
2020-09-02 19:16:10 -0600

    ran a build

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59052" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50577" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
